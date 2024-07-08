d01707e119f1f46a0b8a269cc6be77fe55d9e7a0
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-07-08 12:29:02 +0300

    Completes Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print ([])
+    elif length == 1:
+        print ([0])
+    else:  
+        fib = ([0, 1])
+        for i in range (2, length):
+            nextValue = fib[-1] + fib[-2]
+            fib.append(nextValue)
+        print(fib)     
+
+print_fibonacci(10)
+
+
